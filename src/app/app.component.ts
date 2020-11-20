69a282fd286569a223b9faa906c5c6d618e56c67
Nicolas Peters <nicolas.peters@acerta.be>
2020-11-20 16:40:55 +0100

    google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,20 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {GoogleAnalytics} from '@ionic-native/google-analytics/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
+  providers: [GoogleAnalytics],
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private ga: GoogleAnalytics,
   ) {
     this.initializeApp();
   }
@@ -22,6 +25,12 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.ga.startTrackerWithId('2162804483')
+          .then(() => {
+              console.log('Google analytics is ready now');
+          })
+          .catch(e => console.log('Error starting GoogleAnalytics', e));
     });
   }
 }
